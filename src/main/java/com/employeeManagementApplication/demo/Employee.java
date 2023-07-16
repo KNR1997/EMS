313aebb903f682247c573af75c3c8bd10628917d
KNR1997 <kethakaranasinghe@gmail.com>
2023-07-16 21:40:56 +0530

    EMS: Employee class created

---
+++ src/main/java/com/employeeManagementApplication/demo/Employee.java
@@ -0,0 +1,33 @@
+package com.employeeManagementApplication.demo;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private int id;
+    private String name;
+    private int age;
+    private String designation;
+    private String department;
+    private double salary;
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", designation='" + designation + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+    public Employee(int id, String name, int age, String designation, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.designation = designation;
+        this.department = department;
+        this.salary = salary;
+    }
+}
